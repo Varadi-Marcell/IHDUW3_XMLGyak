4e52c3df2ffab5ac692c847cad6c59eb6bcf5d89
Varadi-Marcell <78903391+Varadi-Marcell@users.noreply.github.com>
2021-09-22 16:20:23 +0200

    Add files via upload

---
+++ IHDUW3_0908/feldolgozo.php
@@ -0,0 +1,19 @@
+<html>
+<body>
+<div align=center>
+<h3>HTML5 Űrlap</h3>	
+</div>
+<div align="center">
+Nev <?php echo $_GET["name"]; ?><br>
+Pin_kod: <?php echo $_GET["pin"]; ?><br>
+Kedvenc_gyumolcs: <?php echo $_GET["fruit"]; ?><br>
+Eletkor: <?php echo $_GET["age"]; ?><br>
+Labmeret: <?php echo $_GET["feet"]; ?><br>
+Onbizalom: <?php echo $_GET["onbizalom"]; ?><br>
+
+</div>
+<a href="urlap_Varadi_Marcell_Balazs.html">vissza az űrlapra</a>
+	
+</body>
+</html>
+
